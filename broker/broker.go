a88d054b287f4d194d801bdafd30b915e1d20fed
Harish <hrsh9486@gmail.com>
2025-11-12 19:19:02 +0000

    Expose AWS private IP on broker

--- broker/broker.go
+++ broker/broker.go
@@ -183,6 +183,12 @@ func main() {
 
 	if *remote == "0" {
 		listenIP = "0.0.0.0"
+		imdsHost := "169.254.169.254"
+		myPrivateIP, err := stubs.GetMyPrivateIP(imdsHost)
+		if err != nil {
+			fmt.Printf("Error retrieving public IP address, with error %v\n ", err)
+		}
+		fmt.Println("Broker running on EC2 instance, with private IP: " + myPrivateIP)
 	} else {
 		listenIP = "127.0.0.1"
 
